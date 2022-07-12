55975c389fe6834ecc503249e195620ee22a98d0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-12 18:31:10 +0100

    Update src/missing/interface.py

--- src/missing/interface.py
+++ src/missing/interface.py
@@ -14,25 +14,8 @@ def main():
     frame = src.missing.features.Features().exc()
     logger.info(frame.head())
 
-    # prevalence features
-    prevalence = pd.melt(frame, id_vars=['iso2', 'N'],
-                         value_vars=['hk_prevalence', 'asc_prevalence', 'tt_prevalence'],
-                         var_name='geohelminth', value_name='missing_fraction')
-    try:
-        prevalence.to_csv(path_or_buf=os.path.join(storage, 'prevalence.csv'),
-                          header=True, index=False, encoding='utf-8')
-    except OSError as err:
-        raise Exception(err.strerror)
-
-    # space & time features
-    spatiotemporal = pd.melt(frame, id_vars=['iso2', 'N'],
-                             value_vars=['site_id', 'latitude', 'longitude', 'year', 'coordinates'],
-                             var_name='feature', value_name='missing_fraction')
-    try:
-        spatiotemporal.to_csv(path_or_buf=os.path.join(storage, 'spatiotemporal.csv'),
-                              header=True, index=False, encoding='utf-8')
-    except OSError as err:
-        raise Exception(err.strerror)
+    src.missing.prevalence.Prevalence(storage=storage).exc(data=frame)
+    src.missing.spatiotemporal.SpatioTemporal(storage=storage).exc(data=frame)
 
 
 if __name__ == '__main__':
@@ -49,11 +32,17 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
     # classes
-    import src.missing.features
     import src.functions.directories
+    import src.missing.features
+    import src.missing.prevalence
+    import src.missing.spatiotemporal
 
-    # Storage
+    # storage
     storage = os.path.join(os.getcwd(), 'warehouse', 'missing')
-    src.functions.directories.Directories().create(path=storage)
+
+    # directories
+    directories = src.functions.directories.Directories()
+    directories.cleanup(storage)
+    directories.create(path=storage)
 
     main()
