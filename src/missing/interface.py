8f44f717f2c1bb6296cf2d5fcf710d43dc9a4f72
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-14 03:36:17 +0100

    Update src/missing/interface.py

--- src/missing/interface.py
+++ src/missing/interface.py
@@ -13,7 +13,6 @@ def main():
     source = os.path.join(str(pathlib.Path(os.getcwd()).parent), 'infections', 'warehouse',
                           'data', 'ESPEN', 'experiments', 'baseline')
     frame = src.missing.features.Features(storage=os.path.join(storage, 'disaggregates'), source=source).exc()
-    logger.info(frame.head())
 
     src.missing.prevalence.Prevalence(
         storage=os.path.join(storage, 'aggregates')).exc(data=frame)
