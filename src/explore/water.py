4772ce66393c834ba41e50c6ce490709b75bef35
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-18 18:41:40 +0100

    Update src/explore/water.py

--- src/explore/water.py
+++ src/explore/water.py
@@ -1,61 +1,59 @@
-import logging
-import os
-import pathlib
-import sys
+import os.path
 
-import dask.dataframe
 import pandas as pd
+import dask.dataframe
+
+import src.functions.streams
 
 
-def main():
-    """
+class Water:
 
-    :return:
-    """
+    def __init__(self, storage):
+        """
 
-    logger.info('prevalence & water')
+        """
 
-    # Reading-in and merging the set of enhanced, and inspected, STH experiments
-    # data files.  Only relevant fields are read.
-    frame = dask.dataframe.read_csv(urlpath=path, usecols=fields, encoding='utf-8')
-    lines: pd.DataFrame = frame.compute()
-    lines.reset_index(drop=True, inplace=True)
-    logger.info(lines.shape)
+        self.storage = storage
+        self.fields = ['iso2', 'year', 'hk_prevalence', 'asc_prevalence', 'tt_prevalence', 'identifier',
+                       'improved_water', 'unpiped_water', 'surface_water', 'piped_water', 'unimproved_water']
 
-    # Writing to a single file
-    src.functions.streams.Streams().write(data=lines, path=os.path.join(storage, 'water.csv'))
+    def __read(self, path):
+        """
 
+        :return:
+        """
 
-if __name__ == '__main__':
+        try:
+            return dask.dataframe.read_csv(urlpath=path, usecols=self.fields, encoding='utf-8')
+        except OSError as err:
+            raise Exception(err.strerror) from err
 
-    # path
-    root = os.getcwd()
-    sys.path.append(root)
-    sys.path.append(os.path.join(root, 'src'))
+    def __write(self, data):
+        """
 
-    # logging
-    logging.basicConfig(level=logging.INFO,
-                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    logger = logging.getLogger(__name__)
+        :param data:
+        :return:
+        """
 
-    # hub
-    hub = pathlib.Path(root).parent
+        return src.functions.streams.Streams().write(
+            data=data, path=os.path.join(self.storage, 'sewer.csv'))
 
-    # source
-    path = os.path.join(hub, 'spatial', 'warehouse', 'features', 'elevation', '*.csv')
-    fields = ['iso2', 'longitude', 'latitude', 'year', 'hk_prevalence', 'asc_prevalence', 'tt_prevalence', 'identifier',
-              'improved_water', 'unpiped_water', 'surface_water', 'piped_water', 'unimproved_water', 'elevation']
+    def exc(self, path):
+        """
 
-    # storage
-    storage = os.path.join(root, 'warehouse', 'explore', 'access')
+        :param path:
+        :return:
+        """
 
-    # classes
-    import src.functions.directories
-    import src.functions.streams
+        frame = self.__read(path=path)
 
-    directories = src.functions.directories.Directories()
-    directories.cleanup(path=storage)
-    directories.create(path=storage)
+        lines: pd.DataFrame = frame.compute()
+        lines.reset_index(drop=True, inplace=True)
+        lines = lines.melt(id_vars=['iso2', 'year', 'identifier', 'hk_prevalence', 'asc_prevalence', 'tt_prevalence'],
+                           value_vars=['improved_water', 'unpiped_water', 'surface_water', 'piped_water', 'unimproved_water'],
+                           var_name='water_facility',
+                           value_name='access_percentage')
 
-    main()
+        message = self.__write(data=lines)
+
+        return message
