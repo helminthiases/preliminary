750a903208c3320009276c00262419642888af4c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-19 14:11:04 +0100

    Update src/explore/water.py

--- src/explore/water.py
+++ src/explore/water.py
@@ -1,7 +1,7 @@
 import os.path
 
-import pandas as pd
 import dask.dataframe
+import pandas as pd
 
 import src.functions.streams
 
@@ -24,9 +24,14 @@ class Water:
         """
 
         try:
-            return dask.dataframe.read_csv(urlpath=path, usecols=self.fields, encoding='utf-8')
+            frame = dask.dataframe.read_csv(urlpath=path, usecols=self.fields, encoding='utf-8')
         except OSError as err:
             raise Exception(err.strerror) from err
+
+        lines: pd.DataFrame = frame.compute()
+        lines.reset_index(drop=True, inplace=True)
+
+        return lines
 
     def __write(self, data):
         """
@@ -38,6 +43,42 @@ class Water:
         return src.functions.streams.Streams().write(
             data=data, path=os.path.join(self.storage, 'water.csv'))
 
+    @staticmethod
+    def __access(data):
+        """
+
+        :param data:
+        :return:
+        """
+
+        lines = data.melt(id_vars=['iso2', 'year', 'identifier', 'hk_prevalence', 'asc_prevalence', 'tt_prevalence'],
+                          value_vars=['improved_water', 'unpiped_water', 'surface_water', 'piped_water', 'unimproved_water'],
+                          var_name='water_facility',
+                          value_name='access_percentage')
+
+        condition = lines[['access_percentage']].notna().values
+        lines = lines.copy().loc[condition, :]
+
+        return lines
+
+    @staticmethod
+    def __prevalence(data):
+        """
+
+        :param data:
+        :return:
+        """
+
+        lines = data.melt(id_vars=['iso2', 'year', 'identifier', 'water_facility', 'access_percentage'],
+                          value_vars=['hk_prevalence', 'asc_prevalence', 'tt_prevalence'],
+                          var_name='infection',
+                          value_name='prevalence')
+
+        condition = lines[['prevalence']].notna().values
+        lines = lines.copy().loc[condition, :]
+
+        return lines
+
     def exc(self, path):
         """
 
@@ -45,15 +86,9 @@ class Water:
         :return:
         """
 
-        frame = self.__read(path=path)
-
-        lines: pd.DataFrame = frame.compute()
-        lines.reset_index(drop=True, inplace=True)
-        lines = lines.melt(id_vars=['iso2', 'year', 'identifier', 'hk_prevalence', 'asc_prevalence', 'tt_prevalence'],
-                           value_vars=['improved_water', 'unpiped_water', 'surface_water', 'piped_water', 'unimproved_water'],
-                           var_name='water_facility',
-                           value_name='access_percentage')
-
+        lines = self.__read(path=path)
+        lines = self.__access(data=lines)
+        lines = self.__prevalence(data=lines)
         message = self.__write(data=lines)
 
         return message
