5a0caa22a1c3b3d845c3850ddb79f3861f329571
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-21 15:36:57 +0100

    Update src/missing/regression/interface.py

--- src/missing/regression/interface.py
+++ src/missing/regression/interface.py
@@ -1,3 +1,6 @@
+"""
+interface
+"""
 import logging
 import os
 import sys
@@ -5,20 +8,29 @@ import pathlib
 
 
 def main():
+    """
+    Null regression
+
+    :return:
+    """
 
     logger.info('pattern')
 
+    # we are interested in the missing data of the raw data
     source = os.path.join(str(pathlib.Path(os.getcwd()).parent), 'infections', 'warehouse',
                           'data', 'ESPEN', 'experiments', 'baseline')
 
+    # focusing on the countries with the smallest number of missing data cells
     paths = [os.path.join(source, f'{name}.csv') for name in ['NG', 'TG', 'LR', 'CD', 'UG', 'KE', 'CI', 'ZM', 'MW']]
 
+    # null regression
     estimates = src.missing.regression.estimates.Estimates(paths=paths).exc()
     message = src.missing.regression.preserve.Preserve().exc(estimates=estimates)
     logger.info(message)
 
 
 if __name__ == '__main__':
+
     # path
     root = os.getcwd()
     sys.path.append(root)
