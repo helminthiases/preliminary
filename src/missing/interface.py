e3f8eb6e779ec9a349f142fee4545d7894641c22
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-27 17:44:09 +0100

    Update src/missing/interface.py

--- src/missing/interface.py
+++ src/missing/interface.py
@@ -26,11 +26,12 @@ def main():
     src.missing.spatiotemporal.SpatioTemporal(
         storage=os.path.join(storage, 'aggregates')).exc(data=frame)
 
-    # The location of the data files that encode data missing states
-    source = os.path.join(os.getcwd(), 'warehouse', 'missing', 'disaggregates')
+    # Focusing on the countries with the smallest number of missing data cells
+    paths = [os.path.join(source, f'{name}.csv')
+             for name in ['NG', 'TG', 'LR', 'CD', 'UG', 'KE', 'CI', 'ZM', 'MW']]
 
     # Hence, null regression.
-    estimates = src.missing.regression.estimates.Estimates(source=source).exc()
+    estimates = src.missing.regression.estimates.Estimates(paths=paths).exc()
     message = src.missing.regression.preserve.Preserve().exc(estimates=estimates)
     logger.info(message)
 
