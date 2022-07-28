b317fefe9b0efe9fcf8ff67cd652f4d201d8dd74
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-28 23:40:37 +0100

    Update src/cases/exists.py

--- src/cases/exists.py
+++ src/cases/exists.py
@@ -1,8 +1,14 @@
-
+"""
+Module: exists
+"""
 import pandas as pd
 import dask.dataframe
 
+
 class Exists:
+    """
+    Exists
+    """
 
     def __init__(self):
         """
@@ -12,10 +18,15 @@ class Exists:
         self.fields = ['iso2', 'year', 'hk_prevalence', 'asc_prevalence', 'tt_prevalence', 'sth_prevalence', 'identifier']
         self.infections = ['hk_prevalence', 'asc_prevalence', 'tt_prevalence', 'sth_prevalence']
 
-    def __read(self, path):
+    def __read(self, source: str) -> pd.DataFrame:
+        """
+
+        :param source:
+        :return:
+        """
 
         try:
-            frame = dask.dataframe.read_csv(urlpath=path, usecols=self.fields, encoding='utf-8')
+            frame = dask.dataframe.read_csv(urlpath=source, usecols=self.fields, encoding='utf-8')
         except OSError as err:
             raise Exception(err.strerror) from err
 
@@ -23,9 +34,14 @@ class Exists:
 
         return lines
 
-    def exc(self, path):
+    def exc(self, source: str)-> pd.DataFrame:
+        """
 
-        lines = self.__read(path=path)
+        :param source:
+        :return:
+        """
+
+        lines = self.__read(source=source)
 
         lines.loc[:, self.infections] = lines[self.infections].notna().values
         exists = lines.groupby(by='iso2').agg(hk_prevalence=('hk_prevalence', sum),
